d4a07a65a41c0d5b8b728c73fae893e24cd586fa
Eknath Kadam <eknathkadam@gmail.com>
2022-04-18 11:00:11 +0530

    initial checking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyemailComponent } from './verifyemail/verifyemail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"signup",component:SignupComponent},
+  {path:"signin", component:SigninComponent},
+  {path:"forgotpassword",component:ForgotpasswordComponent},
+  {path:"verifyemail",component:VerifyemailComponent},
+  {path:"logout",component:LogoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { VerifyemailComponent } from './verifyemail/verifyemail.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ForgotpasswordComponent,
+    VerifyemailComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
